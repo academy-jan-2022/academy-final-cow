b7c1eea92c726f825c7eb290d2fd1926d944da09
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-01 11:16:31 +0100

    sonarcloud fixes

--- src/pages/tests/SecondPage.test.tsx
+++ src/pages/tests/SecondPage.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./SecondPage";
+import App from "../SecondPage";
 
 test("renders learn react link", () => {
   render(<App />);
