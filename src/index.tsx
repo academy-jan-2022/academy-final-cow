06ef62996147eacb69ca018a824f708381772758
Brian Hoang <39828877+bhoang93@users.noreply.github.com>
2022-04-01 11:32:57 +0100

    test branch permissions (#5)
    * test branch permissions
    
    * fixed tests
    
    * conditional on actions
    
    * added tests
    
    * sonar cloud config
    
    * sonar config
    
    * update test
    
    * update test
    
    * sonarcloud config
    
    * sonarcloud config
    
    * sonarcloud fixes
    
    * sonarcloud fixes
    
    * sonarcloud fixes
    
    * sonarcloud fixes
    
    Co-authored-by: Brian Hoang <brian.hd.hoang@codurance.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
