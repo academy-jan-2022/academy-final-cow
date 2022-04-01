948ab912b951f077da6ee555ec021aa3e6615f12
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-01 10:14:26 +0100

    fixed tests

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
-import { Link } from "react-router-dom";
+import React from "react";
+import logo from "../logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Link to="/second-page">Second page</Link>
+        <a href="/second-page">Second page</a>
       </header>
     </div>
   );
