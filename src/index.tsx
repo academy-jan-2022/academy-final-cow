f17daeaca39b775236a65302e110412471cdc730
Simon <simonbesteiro@gmail.com>
2022-04-04 15:32:58 +0200

    downgrade react to version 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
 import SecondPage from "./pages/SecondPage";
-import * as ReactDOMClient from "react-dom/client";
+import ReactDOM from "react-dom";
 
-const container = document.getElementById("root") || new DocumentFragment();
-const root = ReactDOMClient.createRoot(container);
-
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/second-page" element={<SecondPage />} />
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/second-page" element={<SecondPage/>} />
+                <Route path="/" element={<App/>}/>
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
 reportWebVitals();
