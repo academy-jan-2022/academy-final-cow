23351b8f9ce91eaf8272d941d8dc0089a22c3cb3
Simon <simonbesteiro@gmail.com>
2022-04-04 16:04:57 +0200

    red

--- src/pages/tests/HeartBeat.test.tsx
+++ src/pages/tests/HeartBeat.test.tsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {act, render, screen} from "@testing-library/react";
 import Heartbeat from "../Heartbeat";
+const axios = require("axios")
+import client from "../../Services/HttpClient";
+
+jest.mock("axios");
 
 test("Has health check header", () => {
     render(<Heartbeat />);
     const linkElement = screen.getByText(/Health check/i);
     expect(linkElement).toBeInTheDocument();
+});
+
+test("Show green tick when it receives a 200 code calling to Heartbeat test endpoint", async () => {
+    axios.get.mockImplementation(() => Promise.resolve({status: 200}));
+    await act(async () => {
+        render(<Heartbeat />);
+    });
+    const tickElement = screen.getByRole("greenTick");
+    expect(tickElement).toBeInTheDocument();
 });
