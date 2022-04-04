68bc6ad5ec5a684601fc8bf2599d556e62543b7c
Simon <simonbesteiro@gmail.com>
2022-04-04 18:17:11 +0200

    refactor

--- src/pages/tests/HeartBeat.test.tsx
+++ src/pages/tests/HeartBeat.test.tsx
@@ -5,34 +5,28 @@ import Heartbeat from "../Heartbeat";
 const axios = require("axios");
 jest.mock("axios");
 
-beforeEach(() => {
-  const resp = { data: { status: 200 } };
+const apiCall = async (resCode: number) => {
+  const resp = { data: { status: resCode } };
   axios.get.mockImplementation(() => Promise.resolve(resp));
-});
-
-test("Show header", async () => {
   await act(async () => {
     render(<Heartbeat />);
   });
+};
+
+test("Show header", async () => {
+  await apiCall(200);
   const tickElement = screen.getByText("Health check");
   expect(tickElement).toBeInTheDocument();
 });
 
 test("Show green tick when it receives a 200 code calling to Heartbeat test endpoint", async () => {
-  await act(async () => {
-    render(<Heartbeat />);
-  });
+  await apiCall(200);
   const tickElement = screen.getByRole("greenTick");
   expect(tickElement).toBeInTheDocument();
 });
 
 test("Show green tick when it receives a 200 code calling to Heartbeat test endpoint", async () => {
-  const resp = { data: { status: "500" } };
-  axios.get.mockImplementation(() => Promise.resolve(resp));
-
-  await act(async () => {
-    render(<Heartbeat />);
-  });
+  await apiCall(500);
   const crossElement = screen.getByRole("redCross");
   expect(crossElement).toBeInTheDocument();
 });
