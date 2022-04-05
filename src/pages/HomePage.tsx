48377a7f093235bb633f425ed604656fd9d0b283
Sofia Carballo <sofia.carballo@codurance.com>
2022-04-05 17:30:31 +0200

    Google login button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import logo from "../logo.svg";
+import { useNavigate } from "react-router-dom";
 import "./App.css";
+import GoogleLogin, {
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline
+} from "react-google-login";
 
 function HomePage() {
+  let navigate = useNavigate();
+  const responseGoogle = (
+    res: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    if ("profileObj" in res) {
+      navigate("/teams");
+    }
+  };
+
   return (
-    <div >
-        <button>Login</button>
+    <div>
+      <GoogleLogin
+        clientId="671208548253-hrd5a4nvrk4ovsscfskprsbodn7ate8k.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+      />
     </div>
   );
 }
