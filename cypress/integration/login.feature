b3afddad2ad67c438f6ac7b05e4c0c378124f9eb
Nicola <nicola_rushton@outlook.com>
2022-04-05 16:11:10 +0100

    green somehow?

--- cypress/integration/login.feature
+++ cypress/integration/login.feature
@@ -2,5 +2,12 @@ Feature: Google Login
 
   Scenario: I am a user who is not logged in
 
-    When I go to the homepage
+    When I am on the homepage
     Then I should see a login button
+
+  Scenario: I am a user who is logging in
+
+    Given I am on the homepage
+    When I log in with google
+    Then I am logged in
+    And I am redirected to the TEAMS page
--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -1,9 +1,22 @@
 import { When, Then } from "cypress-cucumber-preprocessor/steps";
 
-When("I go to the homepage", () => {
+When("I am on the homepage", () => {
   cy.visit("localhost:3000");
 });
 
 Then("I should see a login button", (url) => {
   cy.get("button").contains("Login");
+});
+
+
+When("I log in with google", (url) => {
+  cy.loginWithGoogleApi()
+});
+
+Then("I am logged in", (url) => {
+  console.log(window.localStorage.getItem('googleCypress'))
+});
+
+Then("I am redirected to the TEAMS page", (url) => {
+  cy.url().should('include', '/teams')
 });
