6bf07900948f15b10a4651a816d71fc454c1e2af
Sofia Carballo <sofia.carballo@codurance.com>
2022-04-06 12:45:55 +0200

    refactor

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -8,7 +8,7 @@ Then("I should see a login button", (url) => {
   cy.get("button").contains("Login");
 });
 
-When("I log in with google authentication", (url) => {
+When("I log in with google authentication again", (url) => {
   cy.loginWithGoogleApi();
 });
 
