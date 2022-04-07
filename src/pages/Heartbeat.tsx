52e696bfea49531f46ce7a7d714345a6cb0ebe63
iagardi <garbigz@gmail.com>
2022-04-07 10:37:46 +0100

    extended heartbeat to read from backend

--- src/pages/Heartbeat.tsx
+++ src/pages/Heartbeat.tsx
@@ -3,17 +3,18 @@ import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
 import CancelIcon from "@mui/icons-material/Cancel";
 import client from "../services/HttpClient";
 
-interface HeartbeatRespose {
+interface HeartbeatResponse {
   status: String;
 }
 
 function Heartbeat() {
   const [backendStateIsHealthy, setBackendState] = useState(false);
+  const [databaseStatus, setDatabaseStatus] = useState(false);
 
   useEffect(() => {
     client
-      .get<HeartbeatRespose>({ url: `${process.env.REACT_APP_HEARTBEAT_URL}` })
-      .then((resp: HeartbeatRespose) => {
+      .get<HeartbeatResponse>({ url: `${process.env.REACT_APP_HEARTBEAT_URL}` })
+      .then((resp: HeartbeatResponse) => {
         if (resp.status === "UP") setBackendState(true);
       });
   }, []);
@@ -23,10 +24,18 @@ function Heartbeat() {
       <h1>Health check</h1>
       {backendStateIsHealthy ? (
         <div role="greenTick">
+          <p>Backend status:</p>
           <DoneOutlineIcon />
+          {databaseStatus ? (
+              <div role="databaseStatus">
+                <p>Database status:</p>
+                <DoneOutlineIcon />
+              </div>
+          ) : null}
         </div>
       ) : (
         <div role="redCross">
+          <p>Backend status:</p>
           <CancelIcon />
         </div>
       )}
--- src/pages/tests/HeartBeat.test.tsx
+++ src/pages/tests/HeartBeat.test.tsx
@@ -5,8 +5,17 @@ import Heartbeat from "../Heartbeat";
 const axios = require("axios");
 jest.mock("axios");
 
-const apiCall = async (resCode: String) => {
-  const resp = { data: { status: resCode } };
+interface HeartbeatResponse {
+  status?: String;
+  components?: {
+    db: {
+      status: String;
+    }
+  }
+}
+
+const apiCall = async (resCode: HeartbeatResponse) => {
+  const resp = { data: resCode };
   axios.get.mockImplementation(() => Promise.resolve(resp));
   await act(async () => {
     render(<Heartbeat />);
@@ -14,19 +23,36 @@ const apiCall = async (resCode: String) => {
 };
 
 test("Show header", async () => {
-  await apiCall("UP");
+  await apiCall({});
   const tickElement = screen.getByText("Health check");
   expect(tickElement).toBeInTheDocument();
 });
 
-test("Show green tick when it receives a 200 code calling to Heartbeat test endpoint", async () => {
-  await apiCall("UP");
+test("Show green tick for backend status when status is up", async () => {
+  await apiCall({
+      status:"UP",
+    });
   const tickElement = screen.getByRole("greenTick");
   expect(tickElement).toBeInTheDocument();
 });
 
-test("Show red tick when it receives a 200 code calling to Heartbeat test endpoint", async () => {
-  await apiCall("DOWN");
+test("Show red tick for backend status when status is down", async () => {
+  await apiCall({
+    status:"DOWN",
+  });
   const crossElement = screen.getByRole("redCross");
   expect(crossElement).toBeInTheDocument();
 });
+
+test("Show green tick for db status when both backend and db status is up", async () => {
+  await apiCall({
+    status: "UP",
+    components: {
+      db: {
+        status: "UP"
+      }
+    }
+  })
+  const dbCrossElement = screen.getByRole("databaseGreenTick");
+  expect(dbCrossElement).toBeInTheDocument();
+})
