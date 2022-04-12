fb29d00757b5f7a7a5ad49507c6a534cbe44446a
Simon <simonbesteiro@gmail.com>
2022-04-12 15:12:31 +0200

    refactor

--- src/services/team/getTeamsByUser.ts
+++ src/services/team/getTeamsByUser.ts
@@ -1,11 +1,8 @@
 import httpClient from "../infrastructure/HttpClient";
 import { Team } from "./Team";
 
-export default function getTeamsByUser() : Promise<Team> {
-
-    let responseData = httpClient.get<Team>({
-        url: process.env.REACT_APP_BASE_URL + "/teams",
-        });
-
-    return responseData;
+export default function getTeamsByUser(): Promise<Team> {
+  return httpClient.get<Team>({
+    url: process.env.REACT_APP_BASE_URL + "/teams",
+  });
 }
