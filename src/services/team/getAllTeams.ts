ba3f4b998c24f48e148c496d572daf59e34c9386
Nicola <nicola_rushton@outlook.com>
2022-04-12 11:27:12 +0100

    green

--- src/services/team/getAllTeams.ts
+++ src/services/team/getAllTeams.ts
@@ -1,6 +1,20 @@
-import { HttpClient } from "../HttpClient";
+import httpClient from "../HttpClient";
 import { Team } from "./Team";
+import {storageHandler} from "../StorageHandler";
 
-export default function getAllTeams() : Promise<Team[]> {
-    throw new Error("boom");
+export default function getAllTeams() : Promise<Team> {
+
+    const tokenObject = storageHandler.getJSONItem("tokenObject")
+    let token = "";
+    if (tokenObject !== null) {
+        token = tokenObject.token_id;
+    }
+
+    let responseData = httpClient.get<Team>({
+        url: process.env.REACT_APP_BASE_URL + "/teams",
+        headers: {
+            token: token,
+        }});
+
+    return responseData;
 }
--- src/services/tests/GetTeamsByUser.test.ts
+++ src/services/tests/GetTeamsByUser.test.ts
@@ -1,21 +1,26 @@
 import getAllTeams from "../team/getAllTeams";
 
 import client from "../HttpClient";
+import {storageHandler} from "../StorageHandler";
 
+jest.mock("../HttpClient");
+const mockedHttpClient = client as jest.Mocked<typeof client>;
+
+jest.mock("../StorageHandler")
+const mockedStorageHandler = storageHandler as jest.Mocked<typeof storageHandler>;
 
 describe("get list of teams that the user is part of", () => {
     test("receive a team", async () => {
         const teamBanana = [{ name: "teamBanana" }];
-        jest.mock("../HttpClient", () => ({
-            get: jest.fn().mockReturnValue(() => Promise.resolve(teamBanana)),
-            post: jest.fn()
-        }));
+        mockedHttpClient.get = jest.fn().mockReturnValue(teamBanana);
+
+        mockedStorageHandler.getJSONItem.mockReturnValue({token_id: "validToken"})
 
         const result = await getAllTeams();
-        expect(client).toBeCalledWith({
-            url: "https://some.url.com/team",
-            header: {
-                token: "something here"
+        expect(mockedHttpClient.get).toBeCalledWith({
+            url: process.env.REACT_APP_BASE_URL + "/teams",
+            headers: {
+                token: "validToken"
             }
         });
         expect(result).toEqual(teamBanana);
