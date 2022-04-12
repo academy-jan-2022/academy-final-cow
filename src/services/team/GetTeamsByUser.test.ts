b6d62bc159f2a80364eaf59f377f26c7a0a62223
Simon <simonbesteiro@gmail.com>
2022-04-12 15:13:45 +0200

    refactor

--- src/services/team/GetTeamsByUser.test.ts
+++ src/services/team/GetTeamsByUser.test.ts
@@ -1,23 +1,20 @@
 import getTeamsByUser from "./getTeamsByUser";
 
 import client from "../infrastructure/HttpClient";
-import {storageHandler} from "../infrastructure/StorageHandler";
 
 jest.mock("../infrastructure/HttpClient");
 const mockedHttpClient = client as jest.Mocked<typeof client>;
 
-jest.mock("../infrastructure/StorageHandler")
-const mockedStorageHandler = storageHandler as jest.Mocked<typeof storageHandler>;
-
 describe("get list of teams that the user is part of", () => {
-    test("receive a team", async () => {
-        const teamBanana = [{ name: "teamBanana" }];
-        mockedHttpClient.get = jest.fn().mockReturnValue(teamBanana);
+  test("receive a team", async () => {
+    const teamBanana = [{ name: "teamBanana" }];
+    mockedHttpClient.get = jest.fn().mockReturnValue(teamBanana);
 
-        const result = await getTeamsByUser();
-        expect(mockedHttpClient.get).toBeCalledWith({
-            url: process.env.REACT_APP_BASE_URL + "/teams"
-        });
-        expect(result).toEqual(teamBanana);
+    const result = await getTeamsByUser();
+
+    expect(mockedHttpClient.get).toBeCalledWith({
+      url: process.env.REACT_APP_BASE_URL + "/teams",
     });
+    expect(result).toEqual(teamBanana);
+  });
 });
