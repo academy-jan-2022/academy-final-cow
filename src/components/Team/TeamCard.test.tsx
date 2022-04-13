16d41e3d541809127baf596728cd050391ee6081
iagardi <garbigz@gmail.com>
2022-04-13 11:37:32 +0100

    red but good red

---
+++ src/components/Team/TeamCard.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {render, screen, waitFor} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+import TeamCard from "./TeamCard";
+
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...(jest.requireActual("react-router-dom") as any),
+    useNavigate: () => mockedUsedNavigate,
+}));
+
+describe("HomePage test should", () => {
+    const teamId = "1"
+    const teamName = "ECA"
+
+    beforeEach(() => {
+        
+        render(
+            <BrowserRouter>
+                    <TeamCard id={teamId} name={teamName} />
+            </BrowserRouter>
+        );
+    });
+
+
+    test('Check if team name is rendered', () => {
+        const teamName = screen.getByText("ECA");
+        expect(teamName).toBeInTheDocument();
+    });
+
+    test('Navigate to team page when clicking on team card', async () => {
+        const card = screen.getByText("ECA");
+        card.click();
+        await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledWith(`teams/${teamId}`));
+    })
+})
