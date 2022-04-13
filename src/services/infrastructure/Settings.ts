d96b18805a02808f90d070ac2703e9b0c80600b9
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-04-13 17:49:21 +0200

    add different enviroments in appSettings

--- src/services/infrastructure/Settings.ts
+++ src/services/infrastructure/Settings.ts
@@ -2,16 +2,18 @@ import appSettings from "../../appSettings.json";
 
 export class Settings {
   private static settings = appSettings;
+  private static enviroment = process.env.NODE_ENV;
 
   public static getApiUrl() {
-    return this.settings.apiUrl;
+    return this.settings[Settings.enviroment].apiUrl
+
   }
 
   public static getGoogleClientId() {
-    return this.settings.googleClientId;
+    return this.settings[Settings.enviroment].googleClientId;
   }
 
   static getHeartBeatUrl() {
-    return this.settings.heartBeatUrl;
+    return this.settings[Settings.enviroment].heartBeatUrl;
   }
 }
