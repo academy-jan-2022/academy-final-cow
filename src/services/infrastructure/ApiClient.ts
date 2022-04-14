51bf5b13f0892bcb0359c8fbc1a83d2e98d1be1f
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-14 13:42:06 +0100

    add route enum

--- src/services/infrastructure/ApiClient.ts
+++ src/services/infrastructure/ApiClient.ts
@@ -1,71 +1,61 @@
-import { AxiosResponse } from "axios";
+import {AxiosResponse} from "axios";
 import {storageHandler} from "./StorageHandler";
 import {Settings} from "./Settings";
 
 const axios = require("axios");
 
 interface AuthResponse {
-  readonly access_token: string;
-  readonly id_token: string;
-  readonly login_hint: string;
-  readonly scope: string;
-  readonly expires_in: number;
-  readonly first_issued_at: number;
-  readonly expires_at: number;
+    readonly access_token: string;
+    readonly id_token: string;
+    readonly login_hint: string;
+    readonly scope: string;
+    readonly expires_in: number;
+    readonly first_issued_at: number;
+    readonly expires_at: number;
 }
 
 class ApiClient {
-  async get<T>(request: GetRequest): Promise<T> {
-    let url =  new URL(Settings.getApiUrl() + request.route);
-    const token = this.getToken();
-    for (let key in request.queryParams) {
-      url.searchParams.append(key, request.queryParams[key]);
+    async get<T>(request: GetRequest): Promise<T> {
+        let url = new URL(Settings.getApiUrl() + request.route);
+        const token = this.getToken();
+        for (let key in request.queryParams) {
+            url.searchParams.append(key, request.queryParams[key]);
+        }
+        const response: AxiosResponse<T> = await axios.get(url.toString(), {headers: {Authorization: `${token}`}});
+        return response.data;
     }
-    const response: AxiosResponse<T> = await axios.get(url.toString(), { headers: { Authorization: `${token}` } });
-    return response.data;
-  }
 
-  async post<T>(request: PostRequest): Promise<T> {
-    let url = new URL(Settings.getApiUrl() + request.route);
-    const token = this.getToken();
-    const response: AxiosResponse<T> = await axios.post(
-      url.toString(),
-      request.body,
-      { headers: { Authorization: `${token}` } }
-    );
-    return response.data;
-  }
+    async post<T>(request: PostRequest): Promise<T> {
+        let url = new URL(Settings.getApiUrl() + request.route);
+        const token = this.getToken();
+        const response: AxiosResponse<T> = await axios.post(
+            url.toString(),
+            request.body,
+            {headers: {Authorization: `${token}`}}
+        );
+        return response.data;
+    }
 
-  getToken(): string | null {
-    const tokenObject: AuthResponse | null = storageHandler.getJSONItem("tokenObject")
-
-    if (tokenObject) {
-      return tokenObject.id_token;
-    } else
-    {return null}
-}
+    getToken(): string | null {
+        const tokenObject: AuthResponse | null = storageHandler.getJSONItem("tokenObject")
+        return tokenObject ? tokenObject.id_token : null;
+    }
 }
 
 export interface GetRequest {
-  route: string;
-  readonly queryParams?: { [name: string]: string };
-  headers?: {
-    token: string;
-  };
+    route: string;
+    readonly queryParams?: { [name: string]: string };
 }
 
 export interface PostRequest {
-  route: string;
-  body: Object;
-  headers?: {
-    token: string;
-  };
+    route: string;
+    body: Object;
 }
 
 export enum ROUTES {
-  HEARTBEAT = "/actuator/health",
-  CREATE_USER = "/login",
-  CREATE_TEAM = "/create-team"
+    HEARTBEAT = "/actuator/health",
+    CREATE_USER = "/login",
+    CREATE_TEAM = "/create-team"
 }
 
 const apiClient = new ApiClient();
