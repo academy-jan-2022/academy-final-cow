5cc95cd023a6986905b7985d98d5fb3370c4e4ad
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-20 10:53:45 +0100

    error page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import PageTemplate from "../TemplatePage/PageTemplate";
+import sadcowboy from "../../images/sadcowboy.png"
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
-const ErrorPage = () => {
-  return (
-    <PageTemplate>
-      <h1 aria-label="title">Error</h1>
-    </PageTemplate>
-  );
+export enum ERRORS {
+    PAGE_NOT_FOUND = "Page not found",
+    SERVER_ERROR = "Something went wrong",
+    UNAUTHORISED = "Unauthorised",
+}
+
+const ErrorPage = ({error}: { error: ERRORS }) => {
+    const navigate = useNavigate();
+
+    return (
+        <PageTemplate>
+            <h1 aria-label="title">Error</h1>
+            <img src={sadcowboy} alt="sad cowboy"/>
+            <p>{error}</p>
+            <Button variant="outlined" onClick={() => navigate("/")}>Return to homepage</Button>
+        </PageTemplate>
+    );
 };
 
 export default ErrorPage;
