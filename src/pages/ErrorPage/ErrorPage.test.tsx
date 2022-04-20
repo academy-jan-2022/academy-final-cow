1ac28b2fa2e5e978df0851ad5b46e08227d2ead5
Brian Hoang <39828877+bhoang93@users.noreply.github.com>
2022-04-20 11:40:36 +0100

    Error handling (#36)
    * test branch permissions
    
    * error page
    
    * error page
    
    * remove console log
    
    * remove console log
    
    Co-authored-by: Brian Hoang <brian.hd.hoang@codurance.com>

--- src/pages/ErrorPage/ErrorPage.test.tsx
+++ src/pages/ErrorPage/ErrorPage.test.tsx
@@ -1,10 +1,42 @@
-import { render, screen } from "@testing-library/react";
-import ErrorPage from "./ErrorPage";
+import {render, screen} from "@testing-library/react";
+import ErrorPage, {ERRORS} from "./ErrorPage";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...(jest.requireActual("react-router-dom") as any),
+    useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("Error page should", () => {
-  test("renders the heading", () => {
-    render(<ErrorPage />);
-    const title = screen.getByRole("heading", { name: "title" });
-    expect(title).toBeInTheDocument();
-  });
+    test("render the heading", () => {
+        render(<ErrorPage error={ERRORS.PAGE_NOT_FOUND}/>);
+        const title = screen.getByRole("heading", {name: "title"});
+        expect(title).toBeInTheDocument();
+    });
+
+    test("display page not found", () => {
+        render(<ErrorPage error={ERRORS.PAGE_NOT_FOUND}/>);
+        const text = screen.getByText("Page not found");
+        expect(text).toBeInTheDocument();
+    });
+
+    test("display error message", () => {
+        render(<ErrorPage error={ERRORS.SOMETHING_WENT_WRONG}/>);
+        const text = screen.getByText("Something went wrong");
+        expect(text).toBeInTheDocument();
+    });
+
+    test("display return to homepage button", () => {
+        render(<ErrorPage error={ERRORS.SOMETHING_WENT_WRONG}/>);
+        const btn = screen.getByText("Return to homepage");
+        expect(btn).toBeInTheDocument();
+    });
+
+    test("return to homepage when the button is clicked", () => {
+        render(<ErrorPage error={ERRORS.SOMETHING_WENT_WRONG}/>);
+        const btn = screen.getByText("Return to homepage");
+        expect(btn).toBeInTheDocument();
+        btn.click();
+        expect(mockedUsedNavigate).toHaveBeenCalledWith("/");
+    });
 });
--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import PageTemplate from "../TemplatePage/PageTemplate";
+import sadcowboy from "../../images/sadcowboy.png"
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import "./error.css"
 
-const ErrorPage = () => {
-  return (
-    <PageTemplate>
-      <h1 aria-label="title">Error</h1>
-    </PageTemplate>
-  );
+export enum ERRORS {
+    PAGE_NOT_FOUND = "Page not found",
+    SOMETHING_WENT_WRONG = "Something went wrong",
+}
+
+const ErrorPage = ({error}: { error: ERRORS }) => {
+    const navigate = useNavigate();
+
+    return (
+        <PageTemplate>
+            <h1 aria-label="title">Error</h1>
+            <img className="sad-cowboy" src={sadcowboy} alt="sad cowboy"/>
+            <p>{error}</p>
+            <Button variant="outlined" onClick={() => navigate("/")}>Return to homepage</Button>
+        </PageTemplate>
+    );
 };
 
 export default ErrorPage;
