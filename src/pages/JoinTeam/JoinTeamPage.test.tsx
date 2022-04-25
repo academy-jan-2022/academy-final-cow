7918132fdd41a24773d5d67bb6fc00a4e27024e6
Nicola <nicola_rushton@outlook.com>
2022-04-25 17:36:33 +0100

    green

--- src/pages/JoinTeam/JoinTeamPage.test.tsx
+++ src/pages/JoinTeam/JoinTeamPage.test.tsx
@@ -3,6 +3,7 @@ import {render, screen, waitFor} from "@testing-library/react";
 import {BrowserRouter} from "react-router-dom";
 import JoinTeamPage from "./JoinTeamPage";
 import * as loginService from "../../services/application/loginService";
+import {storageHandler} from "../../services/infrastructure/StorageHandler";
 
 const LOGIN_BUTTON_TEXT = "Login";
 
@@ -14,6 +15,12 @@ jest.mock("react-router-dom", () => ({
 
 jest.mock("../../services/application/loginService");
 const mockedLoginService = loginService as jest.Mocked<typeof loginService>;
+
+const mockedGetJSONItem = jest.fn()
+
+jest.mock("../../services/infrastructure/StorageHandler");
+const mockedStorageHandler = storageHandler as jest.Mocked<typeof storageHandler>;
+
 
 jest.mock("react-google-login", () => {
     return ({
@@ -35,31 +42,57 @@ jest.mock("react-google-login", () => {
 
 
 describe("join teams page should", () => {
-    beforeEach( () => {
-        render(
-            <BrowserRouter>
-                <JoinTeamPage />
-            </BrowserRouter>
-        );
+
+    afterEach( () => {
+        jest.clearAllMocks();
     })
 
-    test("render log in button", ()=> {
-        const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
-        expect(loginButton).toBeVisible();
+    describe("when user is not logged in", () => {
+
+        beforeEach(() => {
+            mockedStorageHandler.getJSONItem = jest.fn().mockReturnValue(null);
+
+            render(
+                <BrowserRouter>
+                    <JoinTeamPage />
+                </BrowserRouter>
+            );
+        })
+
+        mockedGetJSONItem.mockReturnValue(null);
+
+        test("render log in button", ()=> {
+            const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
+            expect(loginButton).toBeVisible();
+        });
+
+        test("redirects to error page if login is unsuccessful", async () => {
+            mockedLoginService.default.mockResolvedValue(false);
+
+            const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
+            loginButton.click();
+            await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledWith("/error"));
+        });
     });
 
-    test("redirects to error page if login is unsuccessful", async () => {
-        mockedLoginService.default.mockResolvedValue(false);
+    describe("when user is logged in", () => {
 
-        const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
-        loginButton.click();
-        await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledWith("/error"));
-    });
+        beforeEach(() => {
+            const TOKEN_OBJECT = {token: "token"}
+            mockedStorageHandler.getJSONItem = jest.fn().mockReturnValue(TOKEN_OBJECT);
 
-    test("not show the login button if the user is logged in", async () => {
+            render(
+                <BrowserRouter>
+                    <JoinTeamPage />
+                </BrowserRouter>
+            );
+        })
 
-        const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
-        expect(loginButton).not.toBeInTheDocument();
-    });
 
+        test("not show the login button if the user is logged in", async () => {
+
+            const loginButton = screen.queryByText(LOGIN_BUTTON_TEXT);
+            expect(loginButton).not.toBeInTheDocument();
+        });
+    })
 })
--- src/pages/JoinTeam/JoinTeamPage.tsx
+++ src/pages/JoinTeam/JoinTeamPage.tsx
@@ -1,10 +1,20 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import LoginButton from "../../components/LoginButton/LoginButton";
 import {useNavigate} from "react-router-dom";
+import {storageHandler} from "../../services/infrastructure/StorageHandler";
 
 function JoinTeamPage () {
     const navigate = useNavigate()
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const tokenObject = storageHandler.getJSONItem("tokenObject");
+        if (tokenObject) {
+            setIsLoggedIn(true);
+        }
+    }, [] );
 
     const redirectTo = (isSuccessful: boolean) => {
         if (!isSuccessful) {
@@ -12,7 +22,10 @@ function JoinTeamPage () {
         }
     };
     return (
-        <LoginButton handleLoginRedirection={redirectTo}   />)
+        isLoggedIn
+            ? <></>
+            : <LoginButton handleLoginRedirection={redirectTo} />
+            )
 }
 
 export default JoinTeamPage;
