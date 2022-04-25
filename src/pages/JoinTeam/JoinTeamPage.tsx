90ac3b3b67abbac5240f1f15a0f7c49b737baf6b
Nicola <nicola_rushton@outlook.com>
2022-04-25 16:01:59 +0100

    green

--- src/pages/JoinTeam/JoinTeamPage.tsx
+++ src/pages/JoinTeam/JoinTeamPage.tsx
@@ -1,6 +1,20 @@
+import GoogleLogin from "react-google-login";
+import {Settings} from "../../services/infrastructure/Settings";
+import React from "react";
 
 function JoinTeamPage () {
-    return <></>
+
+    const googleLoginHandler = () => {};
+
+    return (
+        <GoogleLogin
+        clientId={`${Settings.getGoogleClientId()}`}
+        buttonText="Login"
+        onSuccess={googleLoginHandler}
+        onFailure={googleLoginHandler}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true}
+    />)
 }
 
 export default JoinTeamPage;
