eea3f8864f39b75afff36ae24394504ad4a7a0b1
Sofia Carballo <sofia.carballo@codurance.com>
2022-04-26 10:26:18 +0200

    red: display title when user is logged in

--- src/pages/JoinTeam/JoinTeamPage.test.tsx
+++ src/pages/JoinTeam/JoinTeamPage.test.tsx
@@ -1,38 +1,37 @@
 import React from "react";
-import {render, screen, waitFor} from "@testing-library/react";
-import {BrowserRouter} from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import JoinTeamPage from "./JoinTeamPage";
 import * as loginService from "../../services/application/loginService";
-import {storageHandler} from "../../services/infrastructure/StorageHandler";
+import { storageHandler } from "../../services/infrastructure/StorageHandler";
 
 const LOGIN_BUTTON_TEXT = "Login";
 
 const mockedUsedNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
     ...(jest.requireActual("react-router-dom") as any),
-    useNavigate: () => mockedUsedNavigate,
+    useNavigate: () => mockedUsedNavigate
 }));
 
 jest.mock("../../services/application/loginService");
 const mockedLoginService = loginService as jest.Mocked<typeof loginService>;
 
-const mockedGetJSONItem = jest.fn()
+const mockedGetJSONItem = jest.fn();
 
 jest.mock("../../services/infrastructure/StorageHandler");
 const mockedStorageHandler = storageHandler as jest.Mocked<typeof storageHandler>;
 
-
 jest.mock("react-google-login", () => {
     return ({
                 onSuccess,
-                buttonText,
+                buttonText
             }: {
         onSuccess: any;
         buttonText: string;
     }) => {
         const handleClick = () => {
             onSuccess({
-                profileObj: { name: "test name" },
+                profileObj: { name: "test name" }
             });
         };
 
@@ -40,28 +39,23 @@ jest.mock("react-google-login", () => {
     };
 });
 
-
 describe("join teams page should", () => {
-
-    afterEach( () => {
+    afterEach(() => {
         jest.clearAllMocks();
-    })
+    });
 
     describe("when user is not logged in", () => {
-
         beforeEach(() => {
             mockedStorageHandler.getJSONItem = jest.fn().mockReturnValue(null);
 
             render(
-                <BrowserRouter>
-                    <JoinTeamPage />
-                </BrowserRouter>
+              <BrowserRouter>
+                  <JoinTeamPage />
+              </BrowserRouter>
             );
-        })
+        });
 
-        mockedGetJSONItem.mockReturnValue(null);
-
-        test("render log in button", ()=> {
+        test("render log in button", () => {
             const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
             expect(loginButton).toBeVisible();
         });
@@ -71,28 +65,35 @@ describe("join teams page should", () => {
 
             const loginButton = screen.getByText(LOGIN_BUTTON_TEXT);
             loginButton.click();
-            await waitFor(() => expect(mockedUsedNavigate).toHaveBeenCalledWith("/error"));
+            await waitFor(() =>
+              expect(mockedUsedNavigate).toHaveBeenCalledWith("/error")
+            );
         });
     });
 
     describe("when user is logged in", () => {
-
         beforeEach(() => {
-            const TOKEN_OBJECT = {token: "token"}
-            mockedStorageHandler.getJSONItem = jest.fn().mockReturnValue(TOKEN_OBJECT);
+            const TOKEN_OBJECT = { token: "token" };
+            mockedStorageHandler.getJSONItem = jest
+              .fn()
+              .mockReturnValue(TOKEN_OBJECT);
 
             render(
-                <BrowserRouter>
-                    <JoinTeamPage />
-                </BrowserRouter>
+              <BrowserRouter>
+                  <JoinTeamPage />
+              </BrowserRouter>
             );
-        })
+        });
+
+        test("render title", () => {
+            const title = screen.getByText("You're being added to a team...");
+            expect(title).toBeInTheDocument();
+        });
 
 
         test("not show the login button if the user is logged in", async () => {
-
             const loginButton = screen.queryByText(LOGIN_BUTTON_TEXT);
             expect(loginButton).not.toBeInTheDocument();
         });
-    })
-})
+    });
+});
