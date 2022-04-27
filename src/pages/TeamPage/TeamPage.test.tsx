c2eed28b738cc377ab917119ffe555575cd1571b
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-04-27 10:42:00 +0200

    refactor: remove duplication

--- src/pages/TeamPage/TeamPage.test.tsx
+++ src/pages/TeamPage/TeamPage.test.tsx
@@ -34,128 +34,101 @@ const aTeamWithMembers: TeamWithMembers = {
 
 const TEAM_PAGE_URL = "/team/1";
 const TEAM_PAGE_ROUTE = "/team/:id";
+let mockedTeamService: jest.Mocked<any>;
 
 describe("Team page should", () => {
-  test("retrieve the team information", async () => {
-    const mockedTeamService = jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
+  describe("on render", () => {
+    beforeEach(async () => {
+      await act(async () => {
+        mockedTeamService = jest
+          .spyOn(teamService, GET_TEAM_METHOD)
+          .mockResolvedValue({ team: aTeamWithMembers });
 
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
+        renderWithMemoryRouter(<TeamPage />, {
+          pageUrl: TEAM_PAGE_URL,
+          route: TEAM_PAGE_ROUTE,
+        });
+      });
     });
 
-    await waitFor(() =>
-      expect(mockedTeamService).toHaveBeenCalledWith(TEAM_ID)
-    );
+    test("retrieve the team information", async () => {
+      await waitFor(() =>
+        expect(mockedTeamService).toHaveBeenCalledWith(TEAM_ID)
+      );
 
-    mockedTeamService.mockRestore();
+      mockedTeamService.mockRestore();
+    });
+
+    test("render the team name as a title", async () => {
+      await waitFor(() =>
+        expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+          TEAM_NAME
+        )
+      );
+
+      mockedTeamService.mockRestore();
+    });
+
+    test("render the team description", async () => {
+      const teamDescription = await screen.findByText(TEAM_DESCRIPTION);
+      expect(teamDescription).toBeInTheDocument();
+
+      mockedTeamService.mockRestore();
+    });
+
+    test("render team members", async () => {
+      const teamMembers = await screen.findByRole("list");
+      expect(teamMembers).toHaveTextContent(USER_ONE_FULL_NAME);
+      expect(teamMembers).toHaveTextContent(USER_TWO_FULL_NAME);
+
+      mockedTeamService.mockRestore();
+    });
+
+    test("render generate join team link button", async () => {
+      const joinButton = await screen.findByText("create join link");
+      expect(joinButton).toBeInTheDocument();
+      mockedTeamService.mockRestore();
+    });
   });
 
-  test("render the team name as a title", async () => {
-    const mockedTeamService = jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
+  describe("on generateLing action", () => {
+    test("calls generate uri on button clicked", async () => {
+      jest
+        .spyOn(teamService, GET_TEAM_METHOD)
+        .mockResolvedValue({ team: aTeamWithMembers });
 
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
+      const mockedGenerateJoinLink = jest
+        .spyOn(teamService, GENERATE_JOIN_LINK)
+        .mockResolvedValue({ link: "mocked url" });
+
+      renderWithMemoryRouter(<TeamPage />, {
+        pageUrl: TEAM_PAGE_URL,
+        route: TEAM_PAGE_ROUTE,
+      });
+
+      const joinButton = await screen.findByText("create join link");
+      await act(async () => await joinButton.click());
+
+      expect(mockedGenerateJoinLink).toHaveBeenCalledWith("1");
     });
 
-    await waitFor(() =>
-      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
-        TEAM_NAME
-      )
-    );
+    test("does not render modal when button has not been clicked", async () => {
+      jest
+        .spyOn(teamService, GET_TEAM_METHOD)
+        .mockResolvedValue({ team: aTeamWithMembers });
 
-    mockedTeamService.mockRestore();
-  });
+      jest
+        .spyOn(teamService, GENERATE_JOIN_LINK)
+        .mockResolvedValue({ link: "http://localhost:3000/join/123456" });
 
-  test("render the team description", async () => {
-    const mockedTeamService = jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
+      renderWithMemoryRouter(<TeamPage />, {
+        pageUrl: TEAM_PAGE_URL,
+        route: TEAM_PAGE_ROUTE,
+      });
 
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
+      const modal = screen.queryByText("http://localhost:3000/join/123456");
+
+      await waitFor(() => expect(modal).not.toBeInTheDocument());
     });
-
-    const teamDescription = await screen.findByText(TEAM_DESCRIPTION);
-    expect(teamDescription).toBeInTheDocument();
-
-    mockedTeamService.mockRestore();
-  });
-
-  test("render team members", async () => {
-    const mockedTeamService = jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
-
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
-    });
-
-    const teamMembers = await screen.findByRole("list");
-    expect(teamMembers).toHaveTextContent(USER_ONE_FULL_NAME);
-    expect(teamMembers).toHaveTextContent(USER_TWO_FULL_NAME);
-
-    mockedTeamService.mockRestore();
-  });
-
-  test("render generate join team link button", async () => {
-    const mockedTeamService = jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
-
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
-    });
-
-    const joinButton = await screen.findByText("create join link");
-    expect(joinButton).toBeInTheDocument();
-    mockedTeamService.mockRestore();
-  });
-
-  test("calls generate uri on button clicked", async () => {
-    jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
-
-    const mockedGenerateJoinLink = jest
-      .spyOn(teamService, GENERATE_JOIN_LINK)
-      .mockResolvedValue({ link: "mocked url" });
-
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
-    });
-
-    const joinButton = await screen.findByText("create join link");
-    await act(async () => await joinButton.click());
-
-    expect(mockedGenerateJoinLink).toHaveBeenCalledWith("1");
-  });
-
-  test("does not render modal when button has not been clicked", async () => {
-    jest
-      .spyOn(teamService, GET_TEAM_METHOD)
-      .mockResolvedValue({ team: aTeamWithMembers });
-
-    jest
-      .spyOn(teamService, GENERATE_JOIN_LINK)
-      .mockResolvedValue({ link: "http://localhost:3000/join/123456" });
-
-    renderWithMemoryRouter(<TeamPage />, {
-      pageUrl: TEAM_PAGE_URL,
-      route: TEAM_PAGE_ROUTE,
-    });
-
-    const modal = screen.queryByText("http://localhost:3000/join/123456");
-
-    await waitFor(() => expect(modal).not.toBeInTheDocument());
   });
 });
