3de0e2b299316e75efc1c2ae07b2d329cfe0d207
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-04-27 13:08:41 +0200

    add MR suggestions

--- src/components/LoginButton/LoginButton.test.tsx
+++ src/components/LoginButton/LoginButton.test.tsx
@@ -1,59 +1,57 @@
-import {render, screen, waitFor} from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import loginService from "../../services/application/loginService";
-import {BrowserRouter} from "react-router-dom";
 
 import React from "react";
 import LoginButton from "./LoginButton";
+import renderWithMemoryRouter from "../../testUtils/renderWithMemoryRouter";
 
 const LOGIN_BUTTON_TEXT = "Login";
 
 jest.mock("../../services/application/loginService");
 
 jest.mock("react-google-login", () => {
-    return ({
-                onSuccess,
-                buttonText,
-            }: {
-        onSuccess: any;
-        buttonText: string;
-    }) => {
-        const handleClick = () => {
-            onSuccess({
-                profileObj: { name: "test name" },
-            });
-        };
-
-        return <button onClick={handleClick}>{buttonText}</button>;
+  return ({
+    onSuccess,
+    buttonText,
+  }: {
+    onSuccess: any;
+    buttonText: string;
+  }) => {
+    const handleClick = () => {
+      onSuccess({
+        profileObj: { name: "test name" },
+      });
     };
+
+    return <button onClick={handleClick}>{buttonText}</button>;
+  };
 });
 
 const handleLoginRedirection = jest.fn();
 
 describe("LoginButton should", () => {
+  let button: HTMLElement;
 
-    beforeEach(() => {
-        render(
-            <BrowserRouter>
-                <LoginButton onLogin={handleLoginRedirection} />
-            </BrowserRouter>
-        );
-    });
+  beforeEach(() => {
+    renderWithMemoryRouter(
+      <LoginButton onLogin={handleLoginRedirection} />,
+      {}
+    );
 
-    test("renders log in button", () => {
-        const button = screen.getByText(LOGIN_BUTTON_TEXT);
-        expect(button).toBeInTheDocument();
-    });
+    button = screen.getByText(LOGIN_BUTTON_TEXT);
+  });
 
-    test("calls login service after clicking login button", async () => {
-        const button = screen.getByText(LOGIN_BUTTON_TEXT);
-        button.click();
-        expect(loginService).toHaveBeenCalled();
-    });
+  test("renders log in button", () => {
+    expect(button).toBeInTheDocument();
+  });
 
-    test("calls handleRedirectionFunction after login service", async () => {
-        const button = screen.getByText(LOGIN_BUTTON_TEXT);
-        button.click();
-        await waitFor(() => expect(handleLoginRedirection).toHaveBeenCalled());
-    })
+  test("calls login service after clicking login button", async () => {
+    button.click();
+    expect(loginService).toHaveBeenCalled();
+  });
 
+  test("calls handleRedirectionFunction after login service", async () => {
+    button.click();
+    await waitFor(() => expect(handleLoginRedirection).toHaveBeenCalled());
+  });
 });
