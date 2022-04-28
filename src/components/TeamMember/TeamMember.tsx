f389c589cfe351d9bde98e3db6d18903120a41d1
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-28 16:23:00 +0100

    green

--- src/components/TeamMember/TeamMember.tsx
+++ src/components/TeamMember/TeamMember.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Box, ListItem } from "@mui/material";
+import avatarGenerator from "../../services/infrastructure/AvatarGenerator";
 
 const TeamMember = ({ fullName }: { fullName: string }) => {
+  const getAvatar = () => {
+    return avatarGenerator.randomise();
+  };
+
   return (
     <Box data-testid="member-container">
+      <img
+        src={getAvatar()}
+        alt="memmber profile image"
+        data-testid="team-member-avatar"
+      />
       <ListItem>{fullName}</ListItem>
     </Box>
   );
