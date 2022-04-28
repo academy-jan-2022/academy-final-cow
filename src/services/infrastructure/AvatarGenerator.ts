ee2840573f3c5e6abc61f05b39a92d4a407030b9
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-28 16:20:42 +0100

    red

---
+++ src/services/infrastructure/AvatarGenerator.ts
@@ -0,0 +1,14 @@
+import avatar1 from "../../images/avatars/da.png";
+import avatar2 from "../../images/avatars/ia.png";
+import avatar3 from "../../images/avatars/sc.png";
+import avatar4 from "../../images/avatars/nr.png";
+
+const avatars = [avatar1, avatar2, avatar3, avatar4];
+
+class AvatarGenerator {
+  randomise() {}
+}
+
+const avatarGenerator = new AvatarGenerator();
+
+export default avatarGenerator;
