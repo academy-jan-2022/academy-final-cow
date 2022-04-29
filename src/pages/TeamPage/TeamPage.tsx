e0d8f1dd5e2604b53e4144b1967a4bd72ca0a3d8
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-29 16:34:09 +0100

    update logo

--- src/pages/TeamPage/TeamPage.tsx
+++ src/pages/TeamPage/TeamPage.tsx
@@ -12,7 +12,7 @@ import ActivitiesContainer from "../../components/ActivitiesContainer/Activities
 
 import TeamMember from "../../components/TeamMember/TeamMember";
 
-import sadcowboy from "../../images/sadcowboy.png";
+import teamlogo from "../../images/team-logo.png";
 import DoubleCheckModal from "../../components/DoubleCheckModal/DoubleCheckModal";
 import { PageRoutes } from "../pageRoutes";
 import avatarGenerator, {
@@ -99,7 +99,7 @@ const TeamPage = () => {
         <Container sx={{ flex: 1 }}>
           <img
             className="team-logo"
-            src={sadcowboy}
+            src={teamlogo}
             alt="team logo"
             data-testid="team-image"
           />
